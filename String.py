1ae55da8aef76fe6a5f319de729e324264bf1c5e
impacevanend <54188811+impacevanend@users.noreply.github.com>
2020-01-13 11:58:21 -0500

    version 2

---
+++ String.py
@@ -0,0 +1,7 @@
+myStr = "Hello World"
+
+#Consultar propiedades y metodos de String
+#print(dir(myStr))
+
+#String mayúsculas
+print(myStr.upper())
---
+++ variable.py
@@ -0,0 +1,31 @@
+name  = "Jose"
+
+print(name)
+
+#Reglas nombres de variables
+
+#case sensitive
+book = "Digital"
+Book = "Digital"
+
+#no puede empezar por numero
+
+# declarar en una sola linea 
+x, book = 100, "I Robot"
+
+print(x)
+print(book)
+
+#Convenciones
+#Snake Case  a_b = ""
+#Camel case aBc = ""
+#Pascal case AbC = ""
+
+#variable constante
+#Se indica en mayúsculas
+PI = 3.1416
+
+#Reasignación de dato en una variable
+bookName = "I Robot"
+bookName = 121212
+
