1c3b0747fcdb60fa678ec1e1403b03d314027c38
impacevanend <54188811+impacevanend@users.noreply.github.com>
2020-01-20 16:09:18 -0500

    versio 11

---
+++ conditionalsIII.py
@@ -0,0 +1,11 @@
+x = 5
+
+if x > 2 and x <10:
+    
+    print(f"El valor de x {x},\nEl valor esta entre 2 y10. ")
+
+if x > 2 or x <=10:
+    print("x is greater than or less or equal to twenty. ")
+
+if(not(z == y)):
+    print("x is not equal to y")
