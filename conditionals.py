2ba78b462a649a92373a6bed564b788e7a0a846a
impacevanend <54188811+impacevanend@users.noreply.github.com>
2020-01-20 16:03:37 -0500

    version 10

--- conditionals.py
+++ conditionals.py
@@ -1,7 +1,17 @@
 
 #TODO control de flujo
 
-x = 20
+x = 30
 
 if x < 30:
-    print("Es correcto")
+
+    print(f"\nx es {x}:\npor lo tanto, no es menor que 20.\n")
+
+elif x == 30:
+
+    print(f"\nx es {x}:\npor lo tanto, es igual a 30.\n")
+
+
+else:
+    print(f"\nx es {x}:\npor lo tanto, no es mayor que 20.\n")
+    
---
+++ conditionalsII.py
@@ -0,0 +1,13 @@
+name = "Jhn"
+lastName = "Carter"
+
+if name == "John":
+
+    if lastName == "Carter":
+       print(f"\nThat is correct.\nthe name is:{name}\nand the last name is: {lastName}.\n ")
+
+    else:
+        print("\nThe data, is not valid\n")
+
+else:
+    print("\n You're other person!\n")
